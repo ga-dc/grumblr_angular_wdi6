01c13441dd7c68dd135e70bbbccf76082dff8de3
Adam Bray <adam.bray@yale.edu>
2015-09-10 17:33:52 -0400

    added inclass on comments

--- js/services/grumble.js
+++ js/services/grumble.js
@@ -1,9 +1,23 @@
 (function() {
   var grumbleServices = angular.module('grumbleServices', ['ngResource']);
-
   grumbleServices.factory('Grumble', ['$resource', function($resource) {
     return $resource('http://grumblr.wdidc.org/grumbles/:id', {}, {
       update: {method: 'PUT'}
     });
   }]);
+
+  grumbleServices.factory('Comment', ['$resource', function($resource) {
+    return $resource('http://grumblr.wdidc.org/grumbles/:grumble_id/comments/:id');
+  }]);
+
+
 })();
+
+Grumble.get({id: 10})
+Comment.query({grumble_id: 55})
+
+// Grumble.update({id: 3}, {title: "Hello!", authorName: "Adam"})
+//
+// Grumble.get({id: 3}, function(grumble){
+//   grumble.$update({title: "Howdy!"})
+// })
