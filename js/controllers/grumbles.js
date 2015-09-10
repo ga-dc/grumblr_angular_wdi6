0248f93affba1493ea2aa6f6eacf29f558edf6e4
Walter <walter.augustine10@gmail.com>
2015-09-09 23:52:05 -0400

    attempted solution

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -2,51 +2,18 @@
   app = angular.module('grumbleControllers',[]);
 
 ////Delete
-  app.controller('grumbleController', ['$routeParams', 'Grumble', function ($routeParams, Grumble) {
+  app.controller('grumbleController', ['$routeParams', '$location', 'Grumble', function ($routeParams, $location, Grumble) {
     this.grumble = Grumble.get({id: $routeParams.id})
-    console.log("here", this)
-    this.delete = function () {
-      console.log("delete happened");
-      console.log("grumble", this.grumble)
-      Grumble.remove({id: this.grumble.id}, function () {
+    this.delete = function (id) {
+      Grumble.delete({id: id}, function(){
         $location.path("/grumbles")
-      });
-    };
-  }]);
-
-
-  app.controller('grumblesController', ['Grumble', function(Grumble) {
-    this.grumbles = Grumble.query()
-    this.reset = function(){
-
-      this.title = ""
-      this.authorName = ""
-      this.content = ""
-      this.photoUrl = ""
-    }
-    this.create = function(){
-      var grumble = {
-        title: this.title,
-        authorName: this.authorName,
-        content: this.content,
-        photoUrl: this.photoUrl
-      }
-      Grumble.save(grumble)
-      self.grumbles = Grumble.query()
-      self.reset()
-    }
-    var self = this
-    this.delete = function(id){
-      Grumble.delete({id: id}, function () {
-        self.grumbles = Grumble.query()
       })
     }
+  }]);
 
-
-    this.showGrumble = true
-    this.toggleForm = function(){
-      this.formIsVisible = this.formIsVisible ? false : true
-    }
+////index
+  app.controller('grumblesController', ['Grumble', function(Grumble) {
+    this.grumbles = Grumble.query();
   }]);
     //////New
   app.controller('newGrumbleController', ['Grumble', '$location', function (Grumble, $location) {
@@ -57,16 +24,15 @@
     }
   }])
 //////edit
-  app.controller('editGrumbleController', ["$location", "$routeParams", 'Grumble', function ($location, $routeParams, Grumble) {
+  app.controller('editGrumbleController', ["$location","$routeParams", 'Grumble', function($location, $routeParams, Grumble){
     this.grumble = Grumble.get({id: $routeParams.id})
-    this.update = function () {
+    this.update = function(){
       this.grumble.$update({id: this.grumble.id});
       $location.path("/grumbles/" + this.grumble.id)
     }
   }])
-
+//////create comment
   app.controller( 'commentsController', function(){
-
     this.create = function( grumble ){
       grumble.comments.unshift({
         content: grumble.newCommentContent
