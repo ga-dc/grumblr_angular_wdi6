cb6d23fcb0e2a64fd91ace2983b2f336d00fad8a
Silvia Brown <siltastic@gmail.com>
2015-09-11 09:45:37 -0400

    Added changes.

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -1,65 +1,35 @@
 (function() {
-  app = angular.module('grumbleControllers',[]);
+  var grumbleControllers = angular.module('grumbleControllers', ['ngRoute'])
 
-  app.controller('grumbleController', ['$routeParams', '$location', 'Grumble', function($routeParams, $location, Grumble){
-    this.grumble = Grumble.get({id: $routeParams.id});
-
-    this.delete = function(){
-      Grumble.delete(this.grumble, function() {
-        $location.path("/grumbles");
-      });
-    };
-  }]);
-
-  app.controller('grumblesController', ['Grumble', function(Grumble) {
-
+  grumbleControllers.controller('grumblesController', ['Grumble', function(Grumble) {
     this.grumbles = Grumble.query();
-
-    this.reset = function(){
-      this.title = "";
-      this.authorName = "";
-      this.content = "";
-      this.photoUrl = "";
-    };
-    this.create = function(){
-      var self = this;
-      Grumble.save({
-        title: this.title,
-        authorName: this.authorName,
-        content: this.content,
-        photoUrl: this.photoUrl
-      }, function(newGrumble) {
-        self.reset();
-        self.grumbles.unshift(newGrumble);
-      });
-    };
-    this.edit = function(index){
-      var grumble = this.grumbles[index];
-      this.title = grumble.title;
-      this.authorName = grumble.authorName;
-      this.photoUrl = grumble.photoUrl;
-      this.content = grumble.content;
-    };
-    this.update = function(index){
-      var grumble = this.grumbles[index];
-      grumble.title = this.title;
-      grumble.authorName = this.authorName;
-      grumble.photoUrl = this.photoUrl;
-      grumble.content = this.content;
-    };
-    this.formIsVisible = false;
-    this.showGrumble = true;
-    this.toggleForm = function(){
-      this.formIsVisible = this.formIsVisible ? false : true;
-    };
   }]);
 
-  app.controller( 'commentsController', function(){
-    this.create = function( grumble ){
-      grumble.comments.unshift({
-        content: grumble.newCommentContent
+  grumbleControllers.controller('grumbleController', ['$routeParams','$location','$http', 'Grumble','Comment', function($routeParams, $location, $http, Grumble, Comment){
+    this.grumble = Grumble.get({id: $routeParams.id}, function(grumble){
+      grumble.comments = Comment.query({grumbleId: $routeParams.id});
+    });
+
+    this.delete = function(id){
+      Grumble.delete({id: id}, function(){
+        $location.path("/grumbles")
       });
-      grumble.newCommentContent = "";
-    };
-  });
+    }
+  }]);
+
+  grumbleControllers.controller('newGrumbleController', ["$location", 'Grumble', function($location, Grumble){
+    this.create = function(){
+      Grumble.save(this.grumble, function(grumble) {
+        $location.path("/grumbles/" + grumble.id);
+      })
+    }
+  }])
+
+  grumbleControllers.controller('editGrumbleController', ["$location","$routeParams", 'Grumble', function($location, $routeParams, Grumble){
+    this.grumble = Grumble.get({id: $routeParams.id})
+    this.update = function(){
+      this.grumble.$update({id: this.grumble.id});
+      $location.path("/grumbles/" + this.grumble.id)
+    }
+  }])
 })();
--- js/routes.js
+++ js/routes.js
@@ -1,14 +1,32 @@
 (function(){
- var router = angular.module('grumbleRouter', []);
- router.config(['$routeProvider', function($routeProvider){
-   $routeProvider.when("/grumbles",{
-     templateUrl: 'js/views/grumbles/index.html',
-     controller: 'grumblesController',
-     controllerAs: 'grumblesCtrl'
-   }).when("/grumbles/:id", {
-     templateUrl: 'js/views/grumbles/show.html',
-     controller: 'grumbleController',
-     controllerAs: 'grumbleCtrl'
-   });
- }]);
+  var router = angular.module('grumbleRouter', []);
+  router.config([
+    '$routeProvider', 
+    function($routeProvider){
+      $routeProvider.
+      when("/grumbles", {
+        templateUrl: 'views/grumbles/index.html',
+        controller: 'grumblesController',
+        controllerAs: 'grumblesCtrl'
+      }).
+      when("/grumbles/new", {
+        templateUrl: 'views/grumbles/new.html',
+        controller: 'newGrumbleController',
+        controllerAs: 'newGrumbleCtrl'
+      }).
+      when("/grumbles/:id", {
+        templateUrl: 'views/grumbles/show.html',
+        controller: 'grumbleController',
+        controllerAs: 'grumbleCtrl'
+      }).
+      when("/grumbles/:id/edit", {
+        templateUrl: 'views/grumbles/edit.html',
+        controller: 'editGrumbleController',
+        controllerAs: 'grumbleCtrl'
+      }).
+      otherwise({
+        redirectTo: "/grumbles"
+      })
+    }
+  ])
 })();
