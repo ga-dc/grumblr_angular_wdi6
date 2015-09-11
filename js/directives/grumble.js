f58e0816e0cf38ffe636089201adf3be25b9c1d5
Lisa <lscottcook@hotmail.com>
2015-09-11 08:52:43 -0400

    saving directive

--- js/directives/grumble.js
+++ js/directives/grumble.js
@@ -4,9 +4,22 @@
     return {
       templateUrl: "views/grumbles/_grumble.html",
       restrict: "EA"
+      replace: true
     }
   });
 })();
 
-// 
+
+(function(){
+  var directives = angular.module('grumbleDirectives',[]);
+  directives.directive('grumbleForm', function(){
+    return {
+      templateUrl: "views/grumbles/_form.html"
+      restrict:  "EA"
+      replace: true
+    }
+  });
+})();
+
+//
 // new and edit
