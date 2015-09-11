f677963597fb137e003154b85055c77492abe062
Robert <i.am.robert.benjamin@gmail.com>
2015-09-10 22:11:57 -0400

    Comments display on show page

--- js/services/grumble.js
+++ js/services/grumble.js
@@ -6,4 +6,8 @@
       update: {method:'PUT'}
     });
   }]);
+
+  grumbleServices.factory('Comment', ['$resource', function($resource) {
+  	return $resource('http://grumblr.wdidc.org/grumbles/:grumble_id/comments/:id')
+  }])
 })();
