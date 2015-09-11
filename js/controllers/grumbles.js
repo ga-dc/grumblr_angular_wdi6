8dc11a04b6d8482b427d46b18eb41509b916cd0c
Erica Andersen <andersenek@gmail.com>
2015-09-11 00:43:30 -0400

    Added Create Comment functionality, started Delete Comment functionality

--- js/controllers/grumbles.js
+++ js/controllers/grumbles.js
@@ -10,15 +10,24 @@
   grumbleControllers.controller('grumbleController', ['$routeParams','$location','$http', 'Grumble', 'Comment', function($routeParams, $location, $http, Grumble, Comment){
 
     this.grumble = Grumble.get({id: $routeParams.id}, function(grumble){ // We can call this id because we said it had one in our route.js .when route path., then pass in the grumble as an argument so we can query the comments on that grumble_id
-          grumble.comments = Comment.query({grumble_id: $routeParams.id});
+      grumble.comments = Comment.query({grumble_id: $routeParams.id});
     });
 
     // create comment method (handles create on show page)
     this.createComment = function(comment){
-      console.log(comment)
-      console.log("click is working")
-
+      Comment.save({grumble_id: $routeParams.id}, comment, function(comment){
+        $location.path("/#/grumbles/" + $routeParams.id)
+      })
      };
+
+    // delete comment method (handles create on show page)
+    this.deleteComment = function(gIndex, cIndex){
+      console.log("delete button works")
+      console.log(gIndex)
+      console.log(cIndex)
+      $location.path("/#/grumbles/" + $routeParams.id)
+
+    };
 
     this.delete = function(id){
       Grumble.delete({id: id}, function(){
